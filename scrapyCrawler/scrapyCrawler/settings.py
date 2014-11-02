745c6f2664c9efeb940d43f36e70ad99eb5f36b0
kenneth <kenneth@9gag.com>
2014-11-02 22:58:09 +0800

    Initial commit after calling 'scrapy startproject scrapyCrawler'

---
+++ scrapyCrawler/scrapyCrawler/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for scrapyCrawler project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'scrapyCrawler'
+
+SPIDER_MODULES = ['scrapyCrawler.spiders']
+NEWSPIDER_MODULE = 'scrapyCrawler.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'scrapyCrawler (+http://www.yourdomain.com)'
