22444ac63c11d22c35b63ae32b54e14eb3a6e374
kenneth <kenneth@9gag.com>
2014-11-11 03:02:35 +0800

    idonate result merger script

---
+++ scrapyCrawler/scripts/idonate-merger.py
@@ -0,0 +1,42 @@
+import sys
+import json
+import codecs
+
+def clean_object(obj):
+    for key in ['source_url', 'source_id', 'source_lang', 'source_name', 'crawl_time']: # , 'name', 'icon' ):
+        del obj[key]
+    return obj
+
+result_dict = {}
+
+def get_meta_dict(obj):
+    return {
+        'source_url' : obj['source_url'],
+        'source_id' : obj['source_id'],
+        'source_name' : obj['source_name'],
+        'source_url' : obj['source_url'],
+        'crawl_time' : obj['crawl_time'],
+        'en' : {},
+        'tc' : {}
+    }
+    
+
+decoder = json.JSONDecoder()
+for i in range(1, 3):
+    for line in open(sys.argv[i]):
+        obj = decoder.decode(line)
+        meta = get_meta_dict(obj)
+        srcId = obj['source_id']
+        srcLang = obj['source_lang']
+        if srcId not in result_dict:
+            result_dict[srcId] = meta
+        result_dict[srcId][srcLang] = dict(result_dict[srcId][srcLang].items() + clean_object(obj).items())
+
+outfile = codecs.open(sys.argv[3], 'w', encoding='utf-8')
+
+encoder = json.JSONEncoder(ensure_ascii=False)
+for key, value in result_dict.iteritems():
+    outfile.write(encoder.encode(value) + '\n')
+
+outfile.close()
+
